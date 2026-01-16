1ba5fd39a68389f5d51d9fe8b8c2bbf02b8703dc
stepdugas <131499172+stepdugas@users.noreply.github.com>
2026-01-15 20:47:14 -0500

    Initial Commit

---
+++ StepsTotalCaloriesWidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StepsTotalCaloriesWidget
+//
+//  Created by Stephanie Dugas on 1/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StepsTotalCaloriesWidget/StepsTotalCaloriesWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepsTotalCaloriesWidgetApp.swift
+//  StepsTotalCaloriesWidget
+//
+//  Created by Stephanie Dugas on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct StepsTotalCaloriesWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
